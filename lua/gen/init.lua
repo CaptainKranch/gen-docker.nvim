d29883161dc75d02848ed9b6496baddf9d4b7dcb
danielgm <daniel.garcia@ezlo.com>
2023-11-05 14:02:50 -0500

    testing: removed some defaults prompts and removed -it flag

--- lua/gen/init.lua
+++ lua/gen/init.lua
@@ -51,7 +51,7 @@ local function get_window_options()
     }
 end
 
-M.command = 'docker exec -it ollama ollama run $model $prompt'
+M.command = 'docker exec ollama ollama run $model $prompt'
 M.model = 'llama2'
 
 M.exec = function(options)
