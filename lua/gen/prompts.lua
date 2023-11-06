eb34c259da2ce37ab376e4aada15e185e5340fb3
danielgm <daniel.garcia@ezlo.com>
2023-11-05 20:19:12 -0500

    Fix: Added deleted prompts

--- lua/gen/prompts.lua
+++ lua/gen/prompts.lua
@@ -1,6 +1,10 @@
 return {
-    Generate = {prompt = "$input", replace = true},
     Summarize = {prompt = "Summarize the following text:\n$text"},
+    Ask = {prompt = "Regarding the following text, $input:\n$text"},
+    Change = {
+        prompt = "Change the following text, $input, just output the final text without additional quotes around it:\n$text",
+        replace = true
+    },
     Enhance_Grammar_Spelling = {
         prompt = "Modify the following text to improve grammar and spelling, just output the final text without additional quotes around it:\n$text",
         replace = true
@@ -17,4 +21,21 @@ return {
         prompt = "Render the following text as a markdown list:\n$text",
         replace = true
     },
+    Make_Table = {
+        prompt = "Render the following text as a markdown table:\n$text",
+        replace = true
+    },
+    Review_Code = {
+        prompt = "Review the following code and make concise suggestions:\n```$filetype\n$text\n```"
+    },
+    Enhance_Code = {
+        prompt = "Enhance the following code, only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
+        replace = true,
+        extract = "```$filetype\n(.-)```"
+    },
+    Change_Code = {
+        prompt = "Regarding the following code, $input, only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
+        replace = true,
+        extract = "```$filetype\n(.-)```"
+    }
 }
